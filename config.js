4b290de20f07c63773daae4d48ad04a627b82ce7
abdoukamby94-dev <abdoukamby94@gmail.com>
2026-09-12 11:48:16 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ Site: https://www.movanest.xyz
 
 //~~~~~Setting Global~~~~~//
 
-global.session_id = "" //Input your session id like deploy with railway or heroku. | If you deploy with pterodactyl panel don't add session id.
+global.session_id = "Ovl-MD_gXcL9yVg_SESSION-ID" //Input your session id like deploy with railway or heroku. | If you deploy with pterodactyl panel don't add session id.
 global.prefix = '.' // bot prefix
-global.owner = ["9476XXXXX"] // Add Your Owner number
-global.bot = "9476XXXXX" // Add Your Bot number
+global.owner = ["221785430513"] // Add Your Owner number
+global.bot = "221785430513" // Add Your Bot number
 global.namabot = "X-Crasher" // Add Your Bot name
-global.namaown = "DanuZz" // Add Your Bot owner name
-global.workType = "public" // public, private, groups, inbox
+global.namaown = "ZIZOU" // Add Your Bot owner name
+global.workType = "privé" // public, private, groups, inbox
 global.anticall = true // Anti Call Feature
 
 //~~~~~Status Updated~~~~~//
